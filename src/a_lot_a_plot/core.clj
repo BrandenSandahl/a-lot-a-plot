81362b35ec86be9aa54967a23814a2a91c93a24e
BrandenSandahl <branden157@gmail.com>
2016-03-25 18:53:03 -0400

    Moved HTML Routes into template files.

--- src/a_lot_a_plot/core.clj
+++ src/a_lot_a_plot/core.clj
@@ -1,14 +1,17 @@
 (ns a-lot-a-plot.core
-   (:require [clojure.string :as str] ;this brings in a new lib
+  (:require [clojure.string :as str] ;this brings in a new lib
             [clojure.walk :as walk] ;brings in walk lib 
             [compojure.core :as core]
+            [compojure.route :as route]
             [ring.adapter.jetty :as jetty]
             [ring.middleware.params :as params]
             [ring.util.response :as response]
             [hiccup.core :as h]
             [clojure.java.jdbc :as sql]
-            [hiccup.form :as form])
-   (:gen-class))
+            [hiccup.form :as form]
+            [a-lot-a-plot.views.layout :as layout]
+            [a-lot-a-plot.views.contents :as contents]
+   (:gen-class)))
 
 (defonce server (atom nil))
 
@@ -38,31 +41,34 @@
   (into [] (sql/query spec ["select * from plant order by id desc"])))
 
 (core/defroutes routes
-  (core/GET "/" request
-    (let [params (:params request)])
-    (h/html
-       [:html
-        [:head
-         [:link {:rel "stylesheet" :href "http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css"}]]
-        [:body
-         [:div.container
-          [:form {:action "/add-plant" :method "post" :role "form" :class "row"}
-           [:fieldset
-              [:legend "Enter a plant to allot to your plot"]
-            [:div.row 
-             [:div.form-group.col-md-5 
-              [:label {:for "name"} "name"]
-              [:input.form-control {:id "name" :placeholder "name"}]]]
-            [:div.row
-             [:div.form-group.col-md-5
-              [:label {:for "type"} "type"]
-              [:input.form-control {:id "type" :placeholder "type"}]]]
-            [:div.row
-             [:div.form-group.col-md-5
-              [:label {:for "description"} "description"]
-              [:textarea.form-control {:placeholder "description" :rows "4" :cols "30"}]]]
-            [:div.row
-             (form/submit-button "submit")]]]]]])))
+  (core/GET "/" [] (layout/application "Home" (contents/index)))
+  (route/resources "/"))
+
+;  (core/GET "/" request
+;    (let [params (:params request)])
+;    (h/html
+;       [:html
+;        [:head
+;         [:link {:rel "stylesheet" :href "http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css"}]]
+;        [:body
+;         [:div.container
+;          [:form {:action "/add-plant" :method "post" :role "form" :class "row"}
+;           [:fieldset
+;              [:legend "Enter a plant to allot to your plot"]
+;            [:div.row 
+;             [:div.form-group.col-md-5 
+;              [:label {:for "name"} "name"]
+;              [:input.form-control {:id "name" :placeholder "name"}]]]
+;            [:div.row
+;             [:div.form-group.col-md-5
+;              [:label {:for "type"} "type"]
+;              [:input.form-control {:id "type" :placeholder "type"}]]]
+;            [:div.row
+;             [:div.form-group.col-md-5
+;              [:label {:for "description"} "description"]
+;              [:textarea.form-control {:placeholder "description" :rows "4" :cols "30"}]]]
+;            [:div.row
+;             (form/submit-button "submit")]]]]]])))
     
 
 (defn -main []
---
+++ src/a_lot_a_plot/views/layout.clj
@@ -0,0 +1,15 @@
+(ns a-lot-a-plot.views.layout
+  (:use [hiccup.page :only (html5 include-css include-js)]))
+
+(defn application [title & content]
+  (html5 {:ng-app "my-app" :lang "en"}
+         [:head
+          [:title title]
+          (include-css "http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css")
+          (include-js "http://code.angularjs.org/1.2.3/angular.min.js")
+          (include-js "js/ui-bootstrap-tpls-0.7.0.min.js")
+          (include-js "js/script.js")
+
+          [:body
+           [:div {:class "container"} content]]]))
+
